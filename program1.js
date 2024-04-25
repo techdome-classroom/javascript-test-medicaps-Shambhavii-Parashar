99c57d5ee414dc0c9b49c004bf63605582a258f0
Shambhavi Parashar <shambhaviparashar02@gmail.com>
2024-04-25 13:43:19 +0530

    solution submission

--- program1.js
+++ program1.js
@@ -1,7 +1,19 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+  let charSet = new Set();
+  let left = 0;
+  let maxLen = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (charSet.has(s[right])) {
+      charSet.delete(s[left]);
+      left++;
+    }
+    charSet.add(s[right]);
+    maxLen = Math.max(maxLen, right - left + 1);
+  }
+
+  return maxLen;
 }
 
-module.exports = { longestSubstring };
-
-
+module.exports = { longestSubstring };
--- program2.js
+++ program2.js
@@ -1,6 +1,17 @@
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
+  const set = new Set();
+    for (let num of nums) {
+        if (num > 0) {
+            set.add(num);
+        }
+    }
+    let missing = 1;
+    while (set.has(missing)) {
+        missing++;
+    }
+    return missing;
 
 }
 
